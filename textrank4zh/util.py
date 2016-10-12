5bc0bdc1c8b88088a8756d693bef366a82c4d22f
oldcai <oldcai>
2016-10-12 21:54:15 +0800

    cache and log graph exceptions

--- textrank4zh/util.py
+++ textrank4zh/util.py
@@ -11,70 +11,36 @@ import os
 import math
 import networkx as nx
 import numpy as np
-import sys
+from logging import getLogger
+logger = getLogger(__name__)
 
-try:
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-except:
-    pass
     
 sentence_delimiters = ['?', '!', ';', '？', '！', '。', '；', '……', '…', '\n']
 allow_speech_tags = ['an', 'i', 'j', 'l', 'n', 'nr', 'nrfg', 'ns', 'nt', 'nz', 't', 'v', 'vd', 'vn', 'eng']
 
-PY2 = sys.version_info[0] == 2
-if not PY2:
-    # Python 3.x and up
-    text_type    = str
-    string_types = (str,)
-    xrange       = range
+text_type = str
+string_types = (str,)
+xrange = range
 
-    def as_text(v):  ## 生成unicode字符串
-        if v is None:
-            return None
-        elif isinstance(v, bytes):
-            return v.decode('utf-8', errors='ignore')
-        elif isinstance(v, str):
-            return v
-        else:
-            raise ValueError('Unknown type %r' % type(v))
 
-    def is_text(v):
-        return isinstance(v, text_type)
+def as_text(v):  ## 生成unicode字符串
+    if v is None:
+        return None
+    elif isinstance(v, bytes):
+        return v.decode('utf-8', errors='ignore')
+    elif isinstance(v, str):
+        return v
+    else:
+        raise ValueError('Unknown type %r' % type(v))
 
-else:
-    # Python 2.x
-    text_type    = unicode
-    string_types = (str, unicode)
-    xrange       = xrange
 
-    def as_text(v):
-        if v is None:
-            return None
-        elif isinstance(v, unicode):
-            return v
-        elif isinstance(v, str):
-            return v.decode('utf-8', errors='ignore')
-        else:
-            raise ValueError('Invalid type %r' % type(v))
+def is_text(v):
+    return isinstance(v, text_type)
 
-    def is_text(v):
-        return isinstance(v, text_type)
-
-__DEBUG = None
 
 def debug(*args):
-    global __DEBUG
-    if __DEBUG is None:
-        try:
-            if os.environ['DEBUG'] == '1':
-                __DEBUG = True
-            else:
-                __DEBUG = False
-        except:
-            __DEBUG = False
-    if __DEBUG:
-        print( ' '.join([str(arg) for arg in args]) )
+    logger.debug(' '.join([str(arg) for arg in args]))
+
 
 class AttrDict(dict):
     """Dict that can get attribute by dot"""
@@ -123,7 +89,8 @@ def get_similarity(word_list1, word_list2):
     
     return co_occur_num / denominator
 
-def sort_words(vertex_source, edge_source, window = 2, pagerank_config = {'alpha': 0.85,}):
+
+def sort_words(vertex_source, edge_source, window, pagerank_config):
     """将单词按关键程度从大到小排序
 
     Keyword arguments:
@@ -132,12 +99,12 @@ def sort_words(vertex_source, edge_source, window = 2, pagerank_config = {'alpha
     window          --  一个句子中相邻的window个单词，两两之间认为有边
     pagerank_config --  pagerank的设置
     """
-    sorted_words   = []
-    word_index     = {}
-    index_word     = {}
+    sorted_words = []
+    word_index = {}
+    index_word = {}
     _vertex_source = vertex_source
-    _edge_source   = edge_source
-    words_number   = 0
+    _edge_source = edge_source
+    words_number = 0
     for word_list in _vertex_source:
         for word in word_list:
             if not word in word_index:
@@ -145,25 +112,28 @@ def sort_words(vertex_source, edge_source, window = 2, pagerank_config = {'alpha
                 index_word[words_number] = word
                 words_number += 1
 
-    graph = np.zeros((words_number, words_number))
-    
-    for word_list in _edge_source:
-        for w1, w2 in combine(word_list, window):
-            if w1 in word_index and w2 in word_index:
-                index1 = word_index[w1]
-                index2 = word_index[w2]
-                graph[index1][index2] = 1.0
-                graph[index2][index1] = 1.0
+    if not words_number:
+        return sorted_words
+    try:
+        graph = np.zeros((words_number, words_number))
 
-    debug('graph:\n', graph)
-    
-    nx_graph = nx.from_numpy_matrix(graph)
-    scores = nx.pagerank(nx_graph, **pagerank_config)          # this is a dict
-    sorted_scores = sorted(scores.items(), key = lambda item: item[1], reverse=True)
-    for index, score in sorted_scores:
-        item = AttrDict(word=index_word[index], weight=score)
-        sorted_words.append(item)
+        for word_list in _edge_source:
+            for w1, w2 in combine(word_list, window):
+                if w1 in word_index and w2 in word_index:
+                    index1 = word_index[w1]
+                    index2 = word_index[w2]
+                    graph[index1][index2] = 1.0
+                    graph[index2][index1] = 1.0
+        debug('graph:\n', graph)
 
+        nx_graph = nx.from_numpy_matrix(graph)
+        scores = nx.pagerank(nx_graph, **pagerank_config)
+        sorted_scores = sorted(scores.items(), key=lambda item: item[1], reverse=True)
+        for index, score in sorted_scores:
+            item = AttrDict(word=index_word[index], weight=score)
+            sorted_words.append(item)
+    except Exception as e:
+        logger.exception(e)
     return sorted_words
 
 
@@ -178,24 +148,26 @@ def sort_sentences(sentences, words, sim_func, pagerank_config):
     """
     sorted_sentences = []
     _source = words
-    sentences_num = len(_source)        
-    graph = np.zeros((sentences_num, sentences_num))
-    
-    for x in xrange(sentences_num):
-        for y in xrange(x, sentences_num):
-            similarity = sim_func( _source[x], _source[y] )
-            graph[x, y] = similarity
-            graph[y, x] = similarity
-            
-    nx_graph = nx.from_numpy_matrix(graph)
-    scores = nx.pagerank(nx_graph, **pagerank_config)              # this is a dict
-    sorted_scores = sorted(scores.items(), key = lambda item: item[1], reverse=True)
+    sentences_num = len(_source)
+    if not sentences_num:
+        return sorted_sentences
+    try:
+        graph = np.zeros((sentences_num, sentences_num))
 
-    for index, score in sorted_scores:
-        item = AttrDict(sentence=sentences[index], weight=score, index=index)
-        sorted_sentences.append(item)
+        for x in xrange(sentences_num):
+            for y in xrange(x, sentences_num):
+                similarity = sim_func(_source[x], _source[y])
+                graph[x, y] = similarity
+                graph[y, x] = similarity
+
+        nx_graph = nx.from_numpy_matrix(graph)
+        scores = nx.pagerank(nx_graph, **pagerank_config)
+        sorted_scores = sorted(scores.items(), key=lambda i: i[1], reverse=True)
+
+        for index, score in sorted_scores:
+            item = AttrDict(sentence=sentences[index], weight=score, index=index)
+            sorted_sentences.append(item)
+    except Exception as e:
+        logger.exception(e)
 
     return sorted_sentences
-
-if __name__ == '__main__':
-    pass
